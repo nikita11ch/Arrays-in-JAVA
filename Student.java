308d07922641337f9f4a780515836765e399abe7
Nikita Chaurasia <115834443+nikita11ch@users.noreply.github.com>
2024-02-23 12:43:33 +0530

    Non-primitive type array creation

--- Student.java
+++ Student.java
@@ -1,69 +1,65 @@
+//creating array of non-primitive type
 package Class_Arrays;
 import java.util.Scanner;
 import java.util.Arrays;
- 
-public class Student 
+public class Student
 {
-    public String name;
-    public int id;
+    String name;
+    int id;
 
-    public Student(String name, int id) 
-    {
-        super();
+    public Student(String name, int id) {
         this.name = name;
         this.id = id;
     }
+
     
-public String getName() 
-{
-    return name;
-}
 
-public void setName(String name)
-{
-    this.name = name;
-}
-
-public int getId() 
-{
-    return id;
-}
-
-public void setId(int id) 
-{
-    this.id = id;
-}
-
-@Override
-public String toString() 
-{
-    return "Student [name=" + name + ", id=" + id + "]";
-}
- 
-public static void main(String[] args)
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter how many student object you want to store");
-        int index=sc.nextInt();
-  	Student s[]=new Student[index];
-  	//   0          	1
-  	//[ 1000x	|  2000x	]
-  	
-  	//  abc  1  	def  2
- 	for(int i=0;i<s.length;i++) 
-        {
-	    System.out.println("Enter the name");
-	    String name=sc.next();
-	    System.out.println("Enter the id");
-	    int id=sc.nextInt();
-	    Student s1=new Student(name,id);
-	    	  //    2000x  	def  2
-	    s[i]=s1;
- 	}
- 	
- 	for(int i=0;i<s.length;i++)
-        {
-	    System.out.println(s[i]);
- 	}	
+    public int getId() {
+        return id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + '}';
+    }
+    
+    
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of student to store data :");
+        int size = sc.nextInt();
+        Student []student = new Student[size];
+        
+        for(int i=0;i<student.length;i++)
+        {
+            System.out.println("Enter the student name :");
+            String name = sc.next();
+            System.out.print("Enter roll no. : ");
+            int id = sc.nextInt();
+            Student s = new Student(name,id);
+            
+            student[i] = s;
+        }
+        for (int i=0;i<student.length;i++)
+        {
+            System.out.println(student[i]);
+        }
+        
+        
+    }  
 }
+
+ 
